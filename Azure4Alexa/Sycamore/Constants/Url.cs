6b7ceeb8473a80875134cc49d19f73c4c7724780
Greg Hardin <greg.hardin@live.com>
2018-01-05 23:56:26 -0800

    * Adds constants Url

---
+++ Azure4Alexa/Sycamore/Constants/Url.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Azure4Alexa.Sycamore.Constants
+{
+    public class Url
+    {
+        public static string SycamoreBaseUrl = "https://app.sycamoreeducation.com/api/v1";
+        public static string Me = "/Me";
+        public static string Students = "/Family/{0}/Students";
+        public static string Student = "/Student/{0}";
+        public static string StudentPhoto = "https://app.sycamoreeducation.com/Schools/{0}/Students";
+    }
+}
