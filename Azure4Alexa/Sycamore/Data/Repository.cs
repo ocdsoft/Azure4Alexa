f25a8b4392ada641597692c3cad0ca1783d287c6
Greg Hardin <greg.hardin@live.com>
2018-01-06 12:50:15 -0800

    * Refactored httpClient request into standalone function.

--- Azure4Alexa/Sycamore/Data/Repository.cs
+++ Azure4Alexa/Sycamore/Data/Repository.cs
@@ -28,52 +28,36 @@ namespace Azure4Alexa.Sycamore.Data
 
         public async Task<Me> GetMe()
         {
-            _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _session.User.AccessToken);
-            Me me = new Me();
-            var httpResponseMessage = await _httpClient.GetAsync(Url.Me);
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                //httpResultString = httpResponseMessage.Content.ReadAsStringAsync().Result;
-                var httpResultString = await httpResponseMessage.Content.ReadAsStringAsync();
-                me = JsonConvert.DeserializeObject<Me>(httpResultString);                
-            }
-            
-            httpResponseMessage.Dispose();
-            return me;
-            
+            var json = await GetJsonResult(Url.Me);
+            return JsonConvert.DeserializeObject<Me>(json);
         }
 
         public async Task<Student> GetStudent(int familyID, string studentFirstName)
-        {           
-            IList<StudentBase> students = new List<StudentBase>();
-            Student student = new Student();
-            _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _session.User.AccessToken);
-            
-            var httpResponseMessage = await _httpClient.GetAsync(string.Format(Url.Students,familyID));
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {                
-                var httpResultString = await httpResponseMessage.Content.ReadAsStringAsync();
-                students = JsonConvert.DeserializeObject<List<StudentBase>>(httpResultString);                
-            }
-            httpResponseMessage.Dispose();
-
-            _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _session.User.AccessToken);
+        {
+            var json = await GetJsonResult(string.Format(Url.Students, familyID));
+            var students = JsonConvert.DeserializeObject<List<StudentBase>>(json);           
 
             var studentBase = students.FirstOrDefault(s => s.FirstName == studentFirstName);
-            httpResponseMessage = await _httpClient.GetAsync(string.Format(Url.Students, familyID));
-            if (httpResponseMessage.IsSuccessStatusCode)
-            {
-                var httpResultString = await httpResponseMessage.Content.ReadAsStringAsync();
-                student = JsonConvert.DeserializeObject<Student>(httpResultString);
-            }
-            httpResponseMessage.Dispose();
+            json = await GetJsonResult(string.Format(Url.Student, studentBase.ID));
 
+            var student = JsonConvert.DeserializeObject<Student>(json);
 
             return student;
+        }
 
+        private async Task<string> GetJsonResult(string url)
+        {
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _session.User.AccessToken);
+            string httpResultString = string.Empty;
+            var httpResponseMessage = await _httpClient.GetAsync(url);
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {                
+                httpResultString = await httpResponseMessage.Content.ReadAsStringAsync();                
+            }
+
+            httpResponseMessage.Dispose();
+            return httpResultString;
         }
     }
 }
